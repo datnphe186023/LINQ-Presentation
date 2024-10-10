b3fc76c96f0fb8eb8ae39d78e74098f7947877a5
datnphe186023 <datnphe186023@fpt.edu.vn>
2024-10-10 21:41:58 +0700

    demo for properties

---
+++ NPD/Deferred Execution.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Data Source
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+        //Deferred Execution (Lazy Execution)
+        var query = numbers.Where(n => n > 3);
+
+        numbers.Add(6);
+
+        foreach (var number in query)
+        {
+            Console.WriteLine(number);
+        }
+
+        //Extension Method
+        var result = numbers.SkipLast(2);
+        foreach (var number in result)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
---
+++ NPD/LinqExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPD
+{
+    public static class LinqExtensions
+    {
+        public static IEnumerable<T> SkipLast<T>(this IEnumerable<T> source, int n)
+        {
+            var buffer = new Queue<T>();
+
+            foreach (var item in source)
+            {
+                buffer.Enqueue(item);
+                if (buffer.Count > n)
+                    yield return buffer.Dequeue();
+            }
+        }
+    }
+}
